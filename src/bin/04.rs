62b1b59386d2ffb463c8f20ed5731f4e4532e64c
Andy Henshaw <andy@nexuslabs.dev>
2024-12-04 10:11:50 -0500

    added Grid

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,50 +1,24 @@
-use std::collections::HashMap;
 
 advent_of_code::solution!(4);
-
-type Grid = HashMap<(i32, i32), char>;
-fn make_grid(input: &str) -> (Grid, usize, usize) {
-    let mut grid = Grid::new();
-
-    let rows = input.lines().count();
-    let cols = input.len() / rows;
-    for (col, line) in input.lines().enumerate() {
-        for (row, ch) in line.chars().enumerate() {
-            grid.insert((row as i32, col as i32), ch);
-        }
-    }
-    (grid, rows, cols)
-}
+use advent_of_code::Grid;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (grid, rows, cols) = make_grid(input);
-    let dirs = [
-        (1i32, 0),
-        (1, 1),
-        (0, 1),
-        (-1, 1),
-        (-1, 0),
-        (-1, -1),
-        (0, -1),
-        (1, -1),
+    let grid = Grid::from_str(input);
+
+    let dirs: [(isize, isize); 8] = [
+        (1, 0),(1, 1),(0, 1),(-1, 1),(-1, 0),(-1, -1),(0, -1),(1, -1),
     ];
     let mut count = 0;
 
-    for row in 0..rows {
-        for col in 0..cols {
+    for row in 0..grid.rows {
+        for col in 0..grid.cols {
             for (dr, dc) in dirs {
-                let mut cr = row as i32;
-                let mut cc = col as i32;
-                for ch in ['X', 'M', 'A', 'S'] {
-                    let Some(&gc) = grid.get(&(cr, cc)) else {
-                        break;
-                    };
-                    if gc != ch {
-                        break;
-                    }
-                    if ch == 'S' {
-                        count += 1
-                    }
+                let mut cr = row as isize;
+                let mut cc = col as isize;
+                for ch in "XMAS".chars() {
+                    let Some(&gc) = grid.get(cr, cc) else {break;};
+                    if gc != ch {break;}
+                    if ch == 'S' {count += 1}
                     cr += dr;
                     cc += dc;
                 }
@@ -56,28 +30,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (grid, rows, cols) = make_grid(input);
+    let grid = Grid::from_str(input);
     let mut count = 0;
 
-    for row in 0..rows {
-        for col in 0..cols {
-            let row = row as i32;
-            let col = col as i32;
-            if grid.get(&(row, col)) != Some(&'A') {
-                continue;
-            }
-            let Some(&ul) = grid.get(&(row - 1, col - 1)) else {
-                continue;
-            };
-            let Some(&ur) = grid.get(&(row - 1, col + 1)) else {
-                continue;
-            };
-            let Some(&ll) = grid.get(&(row + 1, col - 1)) else {
-                continue;
-            };
-            let Some(&lr) = grid.get(&(row + 1, col + 1)) else {
-                continue;
-            };
+    for row in 0..grid.rows {
+        for col in 0..grid.cols {
+            let row = row as isize;
+            let col = col as isize;
+            if grid.get(row, col) != Some(&'A') {continue;}
+            let Some(&ul) = grid.get(row - 1, col - 1) else {continue;};
+            let Some(&ur) = grid.get(row - 1, col + 1) else {continue;};
+            let Some(&ll) = grid.get(row + 1, col - 1) else {continue;};
+            let Some(&lr) = grid.get(row + 1, col + 1) else {continue;};
             if ((ul == 'M' && lr == 'S') || (ul == 'S' && lr == 'M'))
                 && ((ll == 'M' && ur == 'S') || (ll == 'S' && ur == 'M'))
             {
@@ -85,7 +49,6 @@ pub fn part_two(input: &str) -> Option<u32> {
             }
         }
     }
-
     Some(count)
 }
 
@@ -101,7 +64,6 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        // let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(9));
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+use std::collections::HashMap;
+
 pub mod template;
 
-// Use this file to add helper functions and additional modules.
+pub struct Grid<T> {
+    container: HashMap<(isize, isize), T>,
+    pub rows: usize,
+    pub cols: usize,
+}
+
+impl Grid<char> {
+    pub fn from_str(input: &str) -> Self {
+        let mut container = HashMap::<(isize, isize), char>::new();
+
+        let rows = input.lines().count();
+        let cols = input.len() / rows;
+        for (col, line) in input.lines().enumerate() {
+            for (row, ch) in line.chars().enumerate() {
+                container.insert((row as isize, col as isize), ch);
+            }
+        }
+        Self { container, rows, cols }
+    }
+}
+
+impl <T> Grid<T> {
+    pub fn get(&self, x: isize, y: isize) -> Option<&T> {
+        self.container.get(&(y, x))
+    }
+}
