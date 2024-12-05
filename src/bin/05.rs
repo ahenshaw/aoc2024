c61ab57418ff554db62bde52aa8ba49b0d76a8ae
Andy Henshaw <andy@nexuslabs.dev>
2024-12-05 08:59:52 -0500

    day 5 completed

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,23 +1,69 @@
+use std::collections::HashSet;
+
 use aoc_parse::{parser, prelude::*};
 use itertools::Itertools;
 
 advent_of_code::solution!(5);
 
+type Rules = HashSet<(u32, u32)>;
+type Pages = Vec<u32>;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let p = parser!(lines(repeat_sep(i32, " ")));
-    let Ok(lines) = p.parse(input) else {
-        return None;
-    };
-    None
+    let (rules, production) = parse_input(input);
+
+    Some(production.iter()
+    .filter(|pages| is_valid(&rules, pages))
+    .map(|pages| pages[pages.len()/2])
+    .sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let p = parser!(lines(repeat_sep(i32, " ")));
-    let Ok(lines) = p.parse(input) else {
-        return None;
-    };
+    let (rules, production) = parse_input(input);
 
-    None
+    Some(production.iter()
+    .filter(|pages| !is_valid(&rules, pages))
+    .map(|pages| {
+        let fixed = fix(&rules, &pages);
+        fixed[fixed.len()/2]
+    })
+    .sum())
+
+}
+
+fn parse_input(input: &str) -> (Rules, Vec<Pages>) {
+    let p = parser!(
+        section(lines(u32 "|" u32))
+        section(lines(repeat_sep(u32, ",")))
+    );
+    let (rules, production) = p.parse(input).unwrap();
+    let rules: Rules = rules.into_iter().collect();
+    (rules, production)
+}
+
+fn is_valid(rules: &Rules, pages: &Pages) -> bool {
+    pages.iter().combinations(2).all(|pair| rules.contains(&(*pair[0], *pair[1])))
+}
+
+fn fix(rules: &Rules, pages: &Pages) -> Pages {
+    let mut pages = pages.clone();
+
+    loop{
+        'inner:for i in 0..pages.len()-1 {
+            for j in i+1..pages.len() {
+                let p1 = pages[i];
+                let p2 = pages[j];
+                if rules.contains(&(p2, p1))  {
+                    pages[j] = p1;
+                    pages[i] = p2;
+                    break 'inner;
+                }
+            }
+        }
+        if is_valid(rules, &pages) {
+            break;
+        }
+    }
+    pages
 }
 
 #[cfg(test)]
@@ -27,13 +73,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(143));
     }
 
     #[test]
     fn test_part_two() {
-        // let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
