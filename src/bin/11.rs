f4177df3b9bccd8bfb3123e248821253cac602bc
Henshaw, Andrew <andrew.henshaw@gtri.gatech.edu>
2024-12-11 11:36:39 -0500

    day 11 improved

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -3,7 +3,6 @@ advent_of_code::solution!(11);
 
 use std::collections::HashMap;
 
-
 pub fn part_one(input: &str) -> Option<isize> {
     solve(input, 25)
 }
@@ -19,25 +18,22 @@ fn solve(input: &str, blinks: usize) -> Option<isize> {
         .collect();
 
     for _ in 0..blinks {
-        for (stone, count) in stones.clone() {
-            if count > 0 {
-                let mut right = stone.to_string();
-                if stone == 0 {
-                    *stones.entry(0).or_default() -= count;
-                    *stones.entry(1).or_default() += count;
-                } else if right.len() % 2 == 0 {
-                    let left = right.split_off(right.len()/2);
-                    let lefty: u64 = left.parse().unwrap();
-                    let right: u64 = right.parse().unwrap();
-                    *stones.entry(stone).or_default() -= count;
-                    *stones.entry(lefty).or_default() += count;
-                    *stones.entry(right).or_default() += count;
-                } else {
-                    *stones.entry(stone).or_default() -= count;
-                    *stones.entry(stone * 2024).or_default() += count;
-                }
+        let mut nextgen = HashMap::<u64, isize>::new();
+        for (stone, count) in stones {
+            let mut right = stone.to_string();
+            if stone == 0 {
+                nextgen.insert(1, count);
+            } else if right.len() % 2 == 0 {
+                let left = right.split_off(right.len() / 2);
+                let lefty: u64 = left.parse().unwrap();
+                let right: u64 = right.parse().unwrap();
+                nextgen.insert(lefty, count);
+                nextgen.insert(right, count);
+            } else {
+                nextgen.insert(stone * 2024, count);
             }
         }
+        stones = nextgen;
     }
     Some(stones.values().sum())
 }
