1d0c3251b5ac6334922634507f85821b8568a62e
Andy Henshaw <andy@nexuslabs.dev>
2024-12-17 10:52:54 -0500

    day 16 part 1

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,30 +1,64 @@
 #![allow(unused_variables)]
 advent_of_code::solution!(16);
+use std::collections::HashMap;
 
 // use aoc_parse::{parser, prelude::*};
 // use itertools::Itertools;
-// use advent_of_code::{Coord, Grid, CARDINAL};
+use advent_of_code::{Coord, Grid, Direction};
 
-pub fn part_one(input: &str) -> Option<u32> {
-    // let p = parser!(lines(repeat_sep(i32, " ")));
-    // let Ok(lines) = p.parse(input) else {
-    //     return None;
-    // };
-    // let grid: Grid<char> = input.chars().collect();
+pub fn part_one(input: &str) -> Option<u64> {
+    let grid: Grid<char> = input.chars().collect();
+    let (start, _) = grid.iter().find(|(pos, ch)| *ch == 'S').unwrap();
+    let mut scores = HashMap::<Coord, u64>::new();
 
-    None
+    let mut best = u64::MAX;
+    search(&grid, start, Direction::East, 0, &mut best, &mut scores);
+    println!("Lowest score: {best}");
+    Some(best)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    // let p = parser!(lines(repeat_sep(i32, " ")));
-    // let Ok(lines) = p.parse(input) else {
-    //     return None;
-    // };
-    // let grid: Grid<char> = input.chars().collect();
-
-
+    88416
     None
 }
+
+fn search(grid: &Grid<char>, pos: Coord, dir: Direction, score: u64, best: &mut u64, scores: &mut HashMap<Coord, u64>) {
+    if scores.contains_key(&pos) {
+        if scores.get(&pos).unwrap() <= &score {
+            return;
+        }
+    }
+    scores.insert(pos, score);
+    let mut grid = grid.clone();
+    grid.container.insert(pos, 'x');
+    for  rot in [0, 1, 3] {
+        let cost = (rot % 2) * 1000;
+        let mut dir = dir.clone();
+        for _ in 0..rot {
+            dir = dir.rot90();
+        }
+        let pos = pos + dir.clone();
+
+        let score = score + cost + 1;
+        match grid.get(&pos) {
+            Some('.') => {
+                if score < *best {
+                    search(&grid, pos, dir, score, best, scores)
+                }
+            },
+            Some('E') => {
+                if score < *best {
+                    *best = score;
+                }
+                println!("{grid}");
+                println!("{score}");
+
+            }
+            _ => (),
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -33,7 +67,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11048));
     }
 
     #[test]
